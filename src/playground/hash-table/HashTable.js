84ae65102ee396aab5a5a66b5a8f90995ac0cdb3
fnatec <bulat.husainov.dev@gmail.com>
2020-12-11 19:49:58 +0300

    add comment

--- src/playground/hash-table/HashTable.js
+++ src/playground/hash-table/HashTable.js
@@ -79,6 +79,8 @@ export default class HashTable {
   }
 
   /**
+   * NOTE: this is simplified version of hash function, not for prod
+   *
    * Hashes the key (both prehash & hash)
    *
    * @param {string} key - key to hash
