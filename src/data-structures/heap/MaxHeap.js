fc5e0269ab06db347d72c3950d6060d6aba36893
fnatec <bulat.husainov.dev@gmail.com>
2021-01-27 16:12:10 +0300

    add maxHeap implementation (native js array based)

---
+++ src/data-structures/heap/MaxHeap.js
@@ -0,0 +1,233 @@
+/**
+ * Max heap implementation with zero-based indexing.
+ * This data structure can be used for implementing Priority Queue ADT.
+ *
+ * Index math:
+ *   left child:  2i + 1
+ *   right child: 2i + 2
+ *   parent:      Math.floor((i - 1) / 2)
+ *
+ * Interface:
+ *   get_size() - return number of nodes stored in heap
+ *   is_empty() - returns true if heap contains no nodes
+ *   insert - puts node to heap
+ *   peek_max - return node with max priority
+ *   extract_max - pops max priority node from the heap
+ *   change_priority(node_idx) - changes priority of node to given value and bubble_up or down
+ *   remove(idx) - removes node by idx
+ *   build_heap(array) - produce a max-heap from an unordered array
+ *   heap_sort - returns sorted array (ASC order for max heap)
+ *   _bubble_up - if node violates heap property -- swap it with parent
+ *   _bubble_down - if node violates heap property -- swap it with child
+ */
+
+/**
+ * TODO (bonus):
+ * - use dynamic array for storage
+ *   - heap_sort should be done in-place
+ */
+
+export default class MaxHeap {
+  constructor() {
+    this._array = []
+  }
+
+  // get_size() - return number of nodes stored in heap
+  getSize() {
+    return this._array.length
+  }
+
+  // is_empty() - returns true if heap contains no nodes
+  isEmpty() {
+    return this._array.length === 0
+  }
+
+  // insert - puts node to heap,
+  // returns index of inserted node
+  insert(value) {
+    this._array.push(value)
+    if (this.getSize() === 1) {
+      return 0
+    }
+
+    const insertedNodeIdx = this.getSize() - 1
+    return this._bubbleUp(insertedNodeIdx)
+  }
+
+  // peek_max - return node with max priority or NULL if heap is empty
+  peekMax() {
+    return this.isEmpty() ? null : this._array[0]
+  }
+
+  // extract_max - pops max priority node from the heap, returns NULL if heap is empty
+  extractMax() {
+    if (this.isEmpty()) {
+      return null
+    }
+
+    const maxNodeValue = this._array[0]
+    const lastNodeValue = this._array.pop()
+    if (!this.isEmpty()) {
+      this._array[0] = lastNodeValue
+      this._bubbleDown(0)
+    }
+    return maxNodeValue
+  }
+
+  // remove(node_idx) - removes node by idx, returns removed value
+  remove(nodeIdx) {
+    if (this.isEmpty()) {
+      throw new Error('heap is empty')
+    }
+    if (nodeIdx < 0 || nodeIdx + 1 > this.getSize()) {
+      throw new Error('nodeIdx is out of bounds')
+    }
+
+    const oldValue = this._array[nodeIdx]
+    const newValue = this._array.pop()
+    if (this.isEmpty() || nodeIdx === this.getSize()) {
+      return oldValue
+    }
+    this._array[nodeIdx] = newValue
+    if (newValue > oldValue) {
+      this._bubbleUp(nodeIdx)
+    } else if (newValue < oldValue) {
+      this._bubbleDown(nodeIdx)
+    }
+    return oldValue
+  }
+
+  // change_priority(node_idx, new_priority) - changes priority of node
+  // to given value and bubble_up or down
+  changePriority(nodeIdx, newPriorityValue) {
+    if (this.isEmpty()) {
+      throw new Error('heap is empty')
+    }
+    if (nodeIdx < 0 || nodeIdx + 1 > this.getSize()) {
+      throw new Error('nodeIdx is out of bounds')
+    }
+
+    const oldValue = this._array[nodeIdx]
+    this._array[nodeIdx] = newPriorityValue
+    if (newPriorityValue > oldValue) {
+      this._bubbleUp(nodeIdx)
+    } else if (newPriorityValue < oldValue) {
+      this._bubbleDown(nodeIdx)
+    }
+  }
+
+  // _bubble_up(node_idx) - if node violates heap property -- swap it with parent,
+  // returns index of node after bubbling
+  _bubbleUp(nodeIdx) {
+    if (this.isEmpty()) {
+      throw new Error('heap is empty')
+    }
+
+    let currNodeIdx = nodeIdx
+    while (currNodeIdx > 0) {
+      const currNodeValue = this._array[currNodeIdx]
+      const parentNodeIdx = Math.floor((currNodeIdx - 1) / 2)
+      const parentNodeValue = this._array[parentNodeIdx]
+      if (currNodeValue > parentNodeValue) {
+        this._swapNodes(currNodeIdx, parentNodeIdx)
+        currNodeIdx = parentNodeIdx
+        continue
+      } else {
+        break
+      }
+    }
+    return currNodeIdx
+  }
+
+  // _bubble_down(node_idx) - if node violates heap property -- swap it with child,
+  // returns index of node after bubbling
+  _bubbleDown(nodeIdx) {
+    if (this.isEmpty()) {
+      throw new Error('heap is empty')
+    }
+
+    let currNodeIdx = nodeIdx
+    const lastNodeIdx = this._array.length - 1
+    while (currNodeIdx < lastNodeIdx) {
+      const currNodeValue = this._array[currNodeIdx]
+      let leftChildIdx = currNodeIdx * 2 + 1
+      let rightChildIdx = currNodeIdx * 2 + 2
+      leftChildIdx = leftChildIdx <= lastNodeIdx ? leftChildIdx : -1
+      rightChildIdx = rightChildIdx <= lastNodeIdx ? rightChildIdx : -1
+      const leftChildValue = leftChildIdx !== -1 ? this._array[leftChildIdx] : null
+      const rightChildValue = rightChildIdx !== -1 ? this._array[rightChildIdx] : null
+
+      const shouldSwapWithLeftChild =
+        leftChildIdx !== -1 && currNodeValue < leftChildValue && leftChildValue >= rightChildValue
+      const shouldSwapWithRightChild =
+        rightChildIdx !== -1 && currNodeValue < rightChildValue
+      if (shouldSwapWithLeftChild) {
+        this._swapNodes(currNodeIdx, leftChildIdx)
+        currNodeIdx = leftChildIdx
+        continue
+      } else if (shouldSwapWithRightChild) {
+        this._swapNodes(currNodeIdx, rightChildIdx)
+        currNodeIdx = rightChildIdx
+        continue
+      } else {
+        break
+      }
+    }
+    return currNodeIdx
+  }
+
+  _swapNodes(nodeIdxA, nodeIdxB) {
+    const temp = this._array[nodeIdxA]
+    this._array[nodeIdxA] = this._array[nodeIdxB]
+    this._array[nodeIdxB] = temp
+  }
+
+  toString() {
+    if (this.getSize() === 0) {
+      return 'heap is empty'
+    }
+
+    let currLevel = 0
+    let resultStr = ''
+    // loop over all nodes in heap
+    for (let i = 0; i < this.getSize();) {
+      let levelStr = ''
+      const levelSize = 2 ** currLevel
+      // loop over nodes on level
+      for (let j = 0; j < levelSize; j++) {
+        const nodeValue = this._array[i + j] !== undefined ? this._array[i + j] : 'X'
+        levelStr += levelStr === '' ? nodeValue : ' ' + nodeValue
+      }
+      resultStr += levelStr + '\n'
+      i += levelSize
+      currLevel += 1
+    }
+    return resultStr
+  }
+}
+
+// build_heap(array) - produce a max-heap from an unordered array
+MaxHeap.buildHeap = (srcArray) => {
+  const maxHeap = new MaxHeap()
+  maxHeap._array = srcArray
+  if (maxHeap.isEmpty()) {
+    return maxHeap
+  }
+
+  const lastParentNodeIdx = Math.floor(maxHeap.getSize() / 2) - 1
+  for (let i = lastParentNodeIdx; i >= 0; i--) {
+    maxHeap._bubbleDown(i)
+  }
+  return maxHeap
+}
+
+// heap_sort(array) - returns sorted array (ASC order for max heap)
+MaxHeap.heapSort = (srcArray) => {
+  const maxHeap = MaxHeap.buildHeap(srcArray)
+  const reverseSortedArray = []
+  while (!maxHeap.isEmpty()) {
+    const max = maxHeap.extractMax()
+    reverseSortedArray.push(max)
+  }
+  return reverseSortedArray.reverse()
+}
---
+++ src/data-structures/heap/__test__/MaxHeap.test.js
@@ -0,0 +1,282 @@
+import MaxHeap from '../MaxHeap'
+
+describe('MaxHeap', () => {
+  it('should create empty MaxHeap', () => {
+    const maxHeap = new MaxHeap()
+    expect(maxHeap._array).not.toBe(null)
+    expect(maxHeap.getSize()).toBe(0)
+    expect(maxHeap.isEmpty()).toBe(true)
+  })
+
+  it('getSize', () => {
+    const maxHeap = new MaxHeap()
+    expect(maxHeap.getSize()).toBe(0)
+
+    maxHeap.insert(1)
+    expect(maxHeap.getSize()).toBe(1)
+
+    maxHeap.extractMax()
+    expect(maxHeap.getSize()).toBe(0)
+  })
+
+  it('isEmpty', () => {
+    const maxHeap = new MaxHeap()
+    expect(maxHeap.isEmpty()).toBe(true)
+
+    maxHeap.insert(1)
+    expect(maxHeap.isEmpty()).toBe(false)
+
+    maxHeap.extractMax()
+    expect(maxHeap.isEmpty()).toBe(true)
+  })
+
+  it('insert', () => {
+    const maxHeap = new MaxHeap()
+    const insertedNodeIdx1 = maxHeap.insert(1)
+    expect(insertedNodeIdx1).toBe(0)
+    const str1 = '1\n'
+    expect(maxHeap.toString()).toBe(str1)
+
+    const insertedNodeIdx2 = maxHeap.insert(5)
+    expect(insertedNodeIdx2).toBe(0)
+    let str2 = ''
+    str2 += '5\n'
+    str2 += '1 X\n'
+    expect(maxHeap.toString()).toBe(str2)
+
+    const insertedNodeIdx3 = maxHeap.insert(3)
+    expect(insertedNodeIdx3).toBe(2)
+    let str3 = ''
+    str3 += '5\n'
+    str3 += '1 3\n'
+    expect(maxHeap.toString()).toBe(str3)
+
+    const insertedNodeIdx4 = maxHeap.insert(4)
+    expect(insertedNodeIdx4).toBe(1)
+    let str4 = ''
+    str4 += '5\n'
+    str4 += '4 3\n'
+    str4 += '1 X X X\n'
+    expect(maxHeap.toString()).toBe(str4)
+  })
+
+  it('peekMax', () => {
+    const maxHeap = new MaxHeap()
+    expect(maxHeap.peekMax()).toBe(null)
+
+    maxHeap.insert(5)
+    expect(maxHeap.peekMax()).toBe(5)
+
+    maxHeap.insert(10)
+    expect(maxHeap.peekMax()).toBe(10)
+
+    maxHeap.insert(3)
+    expect(maxHeap.peekMax()).toBe(10)
+
+    maxHeap.extractMax()
+    expect(maxHeap.peekMax()).toBe(5)
+    maxHeap.extractMax()
+    expect(maxHeap.peekMax()).toBe(3)
+    maxHeap.extractMax()
+    expect(maxHeap.peekMax()).toBe(null)
+  })
+
+  it('extractMax', () => {
+    const maxHeap = new MaxHeap()
+    expect(maxHeap.extractMax()).toBe(null)
+
+    maxHeap.insert(5)
+    expect(maxHeap.extractMax()).toBe(5)
+    expect(maxHeap.extractMax()).toBe(null)
+
+    maxHeap.insert(10)
+    maxHeap.insert(15)
+    maxHeap.insert(5)
+    expect(maxHeap.extractMax()).toBe(15)
+    expect(maxHeap.extractMax()).toBe(10)
+    expect(maxHeap.extractMax()).toBe(5)
+    expect(maxHeap.extractMax()).toBe(null)
+  })
+
+  it('remove', () => {
+    const maxHeap = new MaxHeap()
+    expect(() => maxHeap.remove(0)).toThrow('heap is empty')
+
+    maxHeap.insert(10)
+    expect(() => maxHeap.remove(-1)).toThrow('nodeIdx is out of bounds')
+    expect(() => maxHeap.remove(1)).toThrow('nodeIdx is out of bounds')
+
+    const removedValue1 = maxHeap.remove(0)
+    expect(removedValue1).toBe(10)
+    expect(maxHeap.getSize()).toBe(0)
+
+    maxHeap.insert(10)
+    maxHeap.insert(5)
+    const removedValue2 = maxHeap.remove(0)
+    expect(removedValue2).toBe(10)
+    const str1 = '5\n'
+    expect(maxHeap.toString()).toBe(str1)
+
+    maxHeap.extractMax()
+    expect(maxHeap.getSize()).toBe(0)
+    //
+    maxHeap.insert(10)
+    maxHeap.insert(5)
+    const removedValue3 = maxHeap.remove(1)
+    expect(removedValue3).toBe(5)
+    const str2 = '10\n'
+    expect(maxHeap.toString()).toBe(str2)
+
+    maxHeap.extractMax()
+    expect(maxHeap.getSize()).toBe(0)
+    //
+    maxHeap.insert(15)
+    maxHeap.insert(10)
+    maxHeap.insert(5)
+    const removedValue4 = maxHeap.remove(1)
+    expect(removedValue4).toBe(10)
+    let str3 = ''
+    str3 += '15\n'
+    str3 += '5 X\n'
+    expect(maxHeap.toString()).toBe(str3)
+  })
+
+  it('changePriority', () => {
+    const maxHeap = new MaxHeap()
+    expect(() => maxHeap.changePriority(0)).toThrow('heap is empty')
+
+    maxHeap.insert(10)
+    expect(() => maxHeap.changePriority(-1)).toThrow('nodeIdx is out of bounds')
+    expect(() => maxHeap.changePriority(1)).toThrow('nodeIdx is out of bounds')
+
+    expect(maxHeap.peekMax()).toBe(10)
+    maxHeap.changePriority(0, 10)
+    expect(maxHeap.peekMax()).toBe(10)
+    maxHeap.changePriority(0, 15)
+    expect(maxHeap.peekMax()).toBe(15)
+
+    const maxHeap2 = new MaxHeap()
+    maxHeap2.insert(15)
+    maxHeap2.insert(10)
+    maxHeap2.insert(5)
+    //
+    maxHeap2.changePriority(0, 20)
+    let str1 = ''
+    str1 += '20\n'
+    str1 += '10 5\n'
+    expect(maxHeap2.toString()).toBe(str1)
+    //
+    maxHeap2.changePriority(1, 25)
+    let str2 = ''
+    str2 += '25\n'
+    str2 += '20 5\n'
+    expect(maxHeap2.toString()).toBe(str2)
+    //
+    maxHeap2.changePriority(2, 30)
+    let str3 = ''
+    str3 += '30\n'
+    str3 += '20 25\n'
+    expect(maxHeap2.toString()).toBe(str3)
+
+    const maxHeap3 = new MaxHeap()
+    maxHeap3.insert(15)
+    maxHeap3.insert(10)
+    maxHeap3.insert(5)
+    //
+    maxHeap3.changePriority(0, 3)
+    let str4 = ''
+    str4 += '10\n'
+    str4 += '3 5\n'
+    expect(maxHeap3.toString()).toBe(str4)
+    //
+    maxHeap3.changePriority(1, 2)
+    let str5 = ''
+    str5 += '10\n'
+    str5 += '2 5\n'
+    expect(maxHeap3.toString()).toBe(str5)
+    //
+    maxHeap3.changePriority(2, 1)
+    let str6 = ''
+    str6 += '10\n'
+    str6 += '2 1\n'
+    expect(maxHeap3.toString()).toBe(str6)
+  })
+
+  it('buildHeap', () => {
+    const maxHeap = MaxHeap.buildHeap([])
+    expect(maxHeap.getSize()).toBe(0)
+    expect(maxHeap.toString()).toBe('heap is empty')
+
+    const maxHeap2 = MaxHeap.buildHeap([1])
+    expect(maxHeap2.getSize()).toBe(1)
+    expect(maxHeap2.toString()).toBe('1\n')
+
+    const maxHeap3 = MaxHeap.buildHeap([3, 2, 1])
+    let str1 = ''
+    str1 += '3\n'
+    str1 += '2 1\n'
+    expect(maxHeap3.toString()).toBe(str1)
+
+    const maxHeap4 = MaxHeap.buildHeap([5, 4, 3, 2, 1])
+    let str2 = ''
+    str2 += '5\n'
+    str2 += '4 3\n'
+    str2 += '2 1 X X\n'
+    expect(maxHeap4.toString()).toBe(str2)
+
+    const maxHeap5 = MaxHeap.buildHeap([1, 4, 5, 2, 3])
+    let str3 = ''
+    str3 += '5\n'
+    str3 += '4 1\n'
+    str3 += '2 3 X X\n'
+    expect(maxHeap5.toString()).toBe(str3)
+
+    const maxHeap6 = MaxHeap.buildHeap([1, 4, 5, 2, 3, 6])
+    let str4 = ''
+    str4 += '6\n'
+    str4 += '4 5\n'
+    str4 += '2 3 1 X\n'
+    expect(maxHeap6.toString()).toBe(str4)
+
+    const maxHeap7 = MaxHeap.buildHeap([1, 4, 5, 2, 3, 7, 6])
+    let str5 = ''
+    str5 += '7\n'
+    str5 += '4 6\n'
+    str5 += '2 3 5 1\n'
+    expect(maxHeap7.toString()).toBe(str5)
+  })
+
+  it('heapSort', () => {
+    let arr1 = []
+    arr1 = MaxHeap.heapSort(arr1)
+    expect(arr1).toEqual([])
+
+    let arr2 = [1]
+    arr2 = MaxHeap.heapSort(arr2)
+    expect(arr2).toEqual([1])
+
+    let arr3 = [2, 1]
+    arr3 = MaxHeap.heapSort(arr3)
+    expect(arr3).toEqual([1, 2])
+
+    let arr4 = [3, 2, 1]
+    arr4 = MaxHeap.heapSort(arr4)
+    expect(arr4).toEqual([1, 2, 3])
+
+    let arr5 = [1, 3, 2]
+    arr5 = MaxHeap.heapSort(arr5)
+    expect(arr5).toEqual([1, 2, 3])
+
+    let arr6 = [3, 1, 2]
+    arr6 = MaxHeap.heapSort(arr6)
+    expect(arr6).toEqual([1, 2, 3])
+
+    let arr7 = [2, 2, 3, 1]
+    arr7 = MaxHeap.heapSort(arr7)
+    expect(arr7).toEqual([1, 2, 2, 3])
+
+    let arr8 = [1, 4, 5, 2, 3, 7, 6]
+    arr8 = MaxHeap.heapSort(arr8)
+    expect(arr8).toEqual([1, 2, 3, 4, 5, 6, 7])
+  })
+})
