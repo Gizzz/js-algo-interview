451e8f1fa95ed7992e9030372f6a30fb24b349b4
fnatec <bulat.husainov.dev@gmail.com>
2021-02-06 20:23:05 +0300

    insertionSort: add comparator and stability tests

--- src/algorithms/sorting/insertion-sort/insertionSort.js
+++ src/algorithms/sorting/insertion-sort/insertionSort.js
@@ -3,16 +3,20 @@
  * It uses shifting instead of swapping to reduce time cost.
  */
 
-export default function insertionSort(arr) {
+import Comparator from '../../../helpers/Comparator'
+
+export default function insertionSort(arr, compareFn) {
+  const comparator = new Comparator(compareFn)
+
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i - 1] <= arr[i]) {
+    if (comparator.lte(arr[i - 1], arr[i])) {
       continue
     }
 
     /* eslint-disable no-param-reassign */
     let idxToInsert = i
     const valueToInsert = arr[i]
-    while (idxToInsert > 0 && arr[idxToInsert - 1] > valueToInsert) {
+    while (idxToInsert > 0 && comparator.gt(arr[idxToInsert - 1], valueToInsert)) {
       arr[idxToInsert] = arr[idxToInsert - 1]
       idxToInsert -= 1
     }
