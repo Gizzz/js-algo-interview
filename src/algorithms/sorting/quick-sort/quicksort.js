d901d494580d2640bdfce10b92e19a98fb062dd8
fnatec <bulat.husainov.dev@gmail.com>
2021-02-10 20:25:51 +0300

    quicksort: add 3-way partitioning

--- src/algorithms/sorting/quick-sort/quicksort.js
+++ src/algorithms/sorting/quick-sort/quicksort.js
@@ -10,28 +10,34 @@ const partition = (arr, loIdx, hiIdx) => {
   // array is sorted in-place, so disable eslint rule:
   /* eslint-disable no-param-reassign */
   const pivot = arr[hiIdx]
-  let pivotIdx = loIdx
-  for (let i = loIdx; i < hiIdx; i++) {
-    if (arr[i] < pivot) {
-      swap(arr, i, pivotIdx)
-      pivotIdx += 1
+  let pivotLoIdx = loIdx
+  let pivotHiIdx = hiIdx
+  let currIdx = loIdx
+  while (currIdx <= pivotHiIdx) {
+    if (arr[currIdx] < pivot) {
+      swap(arr, currIdx, pivotLoIdx)
+      pivotLoIdx += 1
+      currIdx += 1
+    } else if (arr[currIdx] > pivot) {
+      swap(arr, currIdx, pivotHiIdx)
+      pivotHiIdx -= 1
+    } else {
+      currIdx += 1
     }
   }
-  swap(arr, pivotIdx, hiIdx)
-  return pivotIdx
+  return [pivotLoIdx, pivotHiIdx]
   /* eslint-enable no-param-reassign */
 }
 
 export default function quicksort(arr, loIdxParam, hiIdxParam) {
   const loIdx = loIdxParam !== undefined ? loIdxParam : 0
   const hiIdx = hiIdxParam !== undefined ? hiIdxParam : arr.length - 1
-  const isRangeInvalid = loIdx > hiIdx
-  const isOneItem = loIdx === hiIdx
-  if (isRangeInvalid || isOneItem) {
+  // if range is invalid or has only one item
+  if (hiIdx <= loIdx) {
     return
   }
 
-  const pivotIdx = partition(arr, loIdx, hiIdx)
-  quicksort(arr, loIdx, pivotIdx - 1)
-  quicksort(arr, pivotIdx + 1, hiIdx)
+  const [pivotLoIdx, pivotHiIdx] = partition(arr, loIdx, hiIdx)
+  quicksort(arr, loIdx, pivotLoIdx - 1)
+  quicksort(arr, pivotHiIdx + 1, hiIdx)
 }
