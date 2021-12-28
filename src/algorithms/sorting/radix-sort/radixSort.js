16d459e40d42ce4db7126c781ca35e790385ca7d
Bulat Khusainov <bulat.husainov.dev@gmail.com>
2021-12-28 18:17:13 +0300

    add radix sort impl

---
+++ src/algorithms/sorting/radix-sort/radixSort.js
@@ -0,0 +1,25 @@
+/**
+ * Implemntation of LSD radix sort.
+ * Expects keys to be positive numbers.
+ * Can sort objects with numeric keys if `getKeyFn` arg is provided.
+ */
+
+import countingSort from '../counting-sort/countingSort'
+
+export default function radixSort(arr, getKeyFn) {
+  let maxKey = -Infinity
+  const getElemKey = (elem) => (getKeyFn === undefined ? elem : getKeyFn(elem))
+  // eslint-disable-next-line no-restricted-syntax
+  for (const elem of arr) {
+    const elemKey = getElemKey(elem)
+    if (elemKey > maxKey) {
+      maxKey = elemKey
+    }
+  }
+
+  for (let divider = 1; divider < maxKey; divider *= 10) {
+    const currDevider = divider
+    const getKeyFnForCountingSort = (elem) => Math.floor(getElemKey(elem) / currDevider) % 10
+    countingSort(arr, getKeyFnForCountingSort)
+  }
+}
